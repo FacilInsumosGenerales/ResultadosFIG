00f3c68e031bae5e2b1e29116ca5791f61a7e84e
Andrea <chavezmunoz.a@northeastern.edu>
2025-03-28 14:54:08 -0500

    feat: suma y saldo en control de proveedores

--- data/administracion.py
+++ data/administracion.py
@@ -15,13 +15,15 @@ def conseguirDataAdministracion(start_date, end_date):
             cp.Proveedor,
             cp.OC_cliente,
             cp.OC_proveedor,
-            cp.Descripcion Descripcion_factura,
+            cp.TRAZA TRAZA_factura,
             cp.Numero_de_documento,
             cp.Fecha_Emision,
             cp.Fecha_Vencimiento,
             cp.Fecha_de_Envio,
+            cp.Moneda Moneda_factura,
             cp.Valor_sin_IGV,
             cp.IGV
+
         FROM movimientos_bancarios mb
         LEFT JOIN pagos_relacionados pr ON mb.TRAZA = pr.Movimiento
         LEFT JOIN comprobantes_de_pago cp ON cp.TRAZA = pr.Comprobante
@@ -34,7 +36,29 @@ def conseguirDataAdministracion(start_date, end_date):
     cursor.execute(query, (start_date, end_date))
     result = cursor.fetchall()
     connection.close()
-    return pd.DataFrame(result)
+
+    dataCompleta = conseguirSaldo(result)
+    return pd.DataFrame(dataCompleta)
+
+
+def conseguirSaldo(data):
+    df = pd.DataFrame(data)
+
+    # Sumar valor_pago agrupado por TRAZA_factura
+    pagos_agrupados = df.groupby("TRAZA_factura")["Valor_pago"].sum().reset_index()
+    pagos_agrupados.rename(columns={"Valor_pago": "Suma_valor_pago"}, inplace=True)
+
+    # Obtener el valor sin IGV y el IGV de una sola fila por TRAZA_factura
+    factura_unica = df.drop_duplicates(subset=["TRAZA_factura"])
+
+    # Combinar ambos DataFrames en uno solo
+    df_final = pd.merge(factura_unica, pagos_agrupados, on="TRAZA_factura")
+
+    # Calcular la columna "Saldo" como Valor_sin_IGV + IGV - suma de valor_pago
+    df_final["Saldo"] = df_final["Valor_sin_IGV"] + df_final["IGV"] - df_final["Suma_valor_pago"]
+
+    # Mostrar el resultado final
+    return df_final
 
 
 ###Clientes
